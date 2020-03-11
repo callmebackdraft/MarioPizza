ef4717b3abd4df556d828d6c1bd1cd19f2422cee
Callmebackdraft <dennis.aspers@gmail.com>
2020-03-11 16:31:59 +0100

    method for categories created

--- MarioImport/MarioImport/Program.cs
+++ MarioImport/MarioImport/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.IO;
+using System.Linq;
 using ExcelDataReader;
 
 namespace MarioImport
@@ -8,15 +11,74 @@ namespace MarioImport
     {
         static void Main(string[] args)
         {
-            string path = "";
-            using (FileStream stream = File.OpenRead(path))
-            using (IExcelDataReader dr = ExcelReaderFactory.CreateReader(stream))
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            string basePath = @"C:\Users\tonyw\source\repos\MarioImport\Data";
+            foreach (string str in GetCategories(basePath))
             {
-                while (dr.Read())
-                {
+                Console.WriteLine(str);
+            }
 
+        }
+
+
+        private static List<string> GetCategories(string basePath)
+        {
+            string[] files = { @"\pizza_ingredienten.xlsx", @"\Overige producten.xlsx" };
+            List<string> result = new List<string>();
+            foreach(string file in files)
+            {
+                using (FileStream stream = File.OpenRead(basePath + file))
+                using (IExcelDataReader dr = ExcelReaderFactory.CreateOpenXmlReader(stream))
+                {
+                    DataSet data = dr.AsDataSet();
+                    var table = data.Tables[0];
+                    int categoryColumn = 0;
+                    int subCatColumn = 0;
+                    for (int rowCount = 0; rowCount < table.Rows.Count; rowCount++)
+                    {
+                        if(rowCount == 0)
+                        {
+                            for(int columnCount = 0; columnCount < table.Rows[rowCount].ItemArray.Length; columnCount++)
+                            {
+                                if (table.Rows[rowCount].ItemArray[columnCount].ToString().ToLower() == "categorie")
+                                {
+                                    categoryColumn = columnCount;
+                                }
+
+                                if (table.Rows[rowCount].ItemArray[columnCount].ToString().ToLower() == "subcategorie")
+                                {
+                                    subCatColumn = columnCount;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (table.Rows[rowCount].ItemArray[categoryColumn].ToString().ToLower().IndexOfAny( new char[] { '&', ',' }) > 0){
+                                result.AddRange(table.Rows[rowCount].ItemArray[categoryColumn].ToString().Split(new char[] { '&', ','}));
+                            }
+                            else
+                            {
+                                result.Add(table.Rows[rowCount].ItemArray[categoryColumn].ToString());
+                            }
+                            
+                            if (table.Rows[rowCount].ItemArray[subCatColumn].ToString().ToLower().IndexOfAny(new char[] { '&', ',' }) > 0)
+                            {
+                                result.AddRange(table.Rows[rowCount].ItemArray[subCatColumn].ToString().Split(new char[] { '&', ',' }));
+                            }
+                            else
+                            {
+                                result.Add(table.Rows[rowCount].ItemArray[subCatColumn].ToString());
+                            }
+                        }
+                    }
                 }
             }
+            return result.Select(t => t.Trim()).Distinct().ToList();
         }
+
+        //private string[] GetProducts()
+        //{
+
+        //}
     }
 }
