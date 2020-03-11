616d8af0e686e77badce0b560c01dcbd7db56159
Callmebackdraft <dennis.aspers@gmail.com>
2020-03-11 14:22:42 +0100

    initial commit

---
+++ MarioImport/MarioImport/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using ExcelDataReader;
+
+namespace MarioImport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "";
+            using (FileStream stream = File.OpenRead(path))
+            using (IExcelDataReader dr = ExcelReaderFactory.CreateReader(stream))
+            {
+                while (dr.Read())
+                {
+
+                }
+            }
+        }
+    }
+}
